cab49aaf5ee64072ae1aafe55d86094c943cd1a5
Taynara Cristina <taynaracris11@hotmail.com>
2019-11-24 18:15:46 -0300

    Create class Predio

---
+++ src/sistema_de_salas/Predios.java
@@ -0,0 +1,42 @@
+package sistema_de_salas;
+
+public class Predios {
+
+	private int numDeSalas;
+	private boolean possuiEstacionamento;
+	private String nome;
+	//private double taxaOcupacao;
+	
+	public void cadastrarSala() {}
+
+	public void removerSala() {}
+
+	public int getNumDeSalas() {
+		return this.numDeSalas;
+	}
+
+	public void setNumDeSalas(int numDeSalas) {
+		this.numDeSalas = numDeSalas;
+	}
+
+	public boolean getPossuiEstacionamento() {
+		return this.possuiEstacionamento;
+	}
+
+	public void setPossuiEstacionamento(boolean possuiEstacionamento) {
+		this.possuiEstacionamento = possuiEstacionamento;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+/*	public double getTaxaOcupacao() {
+		
+	}
+*/
+}
