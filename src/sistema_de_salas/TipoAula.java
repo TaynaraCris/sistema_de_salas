9f576e317a5c45a4c877475156f1829e0fa2abaf
Taynara Cristina <taynaracris11@hotmail.com>
2019-12-07 12:56:28 -0300

    Create class TipoAula

---
+++ src/sistema_de_salas/TipoAula.java
@@ -0,0 +1,33 @@
+package sistema_de_salas;
+
+public class TipoAula {
+
+	private String graduacao;
+	private String posGraduacao;
+	private String extensao;
+
+	public String getGraducao() {
+		return this.graduacao;
+	}
+
+	public void setGraduacao(String graduacao) {
+		this.graduacao = graduacao;
+	}
+
+	public String getPosGraduacao() {
+		return this.posGraduacao;
+	}
+
+	public void setPosGraduacao(String posGraduacao) {
+		this.posGraduacao = posGraduacao;
+	}
+
+	public String getExtensao() {
+		return this.extensao;
+	}
+
+	public void setExtensao(String extensao) {
+		this.extensao = extensao;
+	}
+
+}
