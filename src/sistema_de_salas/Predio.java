80b88473d762042f72059388f2d927917b27ee1c
Taynara Cristina <taynaracris11@hotmail.com>
2019-12-07 12:39:05 -0300

    Create method cadastrarSala and removerSala

--- src/sistema_de_salas/Predio.java
+++ src/sistema_de_salas/Predio.java
@@ -1,13 +1,46 @@
 package sistema_de_salas;
 
+import java.util.Scanner;
+
 public class Predio {
+	
+	Scanner scan = new Scanner(System.in);
 
 	private int numDeSalas;
 	private boolean possuiEstacionamento;
 	private String nome;
-	//private double taxaOcupacao;
+//	private double taxaOcupacao;
 	
-	public void cadastrarSala() {}
+	public void cadastrarSala() {
+	/*	- eLaboratorio: boolean
+		- possuiArCondicionado: boolean
+		- possuiProjetor: boolean
+		- possuiTelevisao: boolean
+		- gradeHoraria: ArrayList<Aula>
+		- taxaOcupacao: double*/
+
+		Sala sala = new Sala();
+		
+		System.out.println("Informe o nome da sala ");
+		String nome = scan.next();
+		sala.setNome(nome);
+		
+		System.out.println("Informe a quantidade de lugares da sala ");
+		int qtdLugares = scan.nextInt();
+		sala.setQtdLugares(qtdLugares);
+/*
+		System.out.println("O prédio possui estacionamento? (sim ou nao)");
+		String temEstacionamento = scan.next();
+		boolean possuiEstacionamento = false;
+		
+		if(temEstacionamento.equalsIgnoreCase("sim")) {
+			possuiEstacionamento = true;
+		}else if(temEstacionamento.equalsIgnoreCase("nao")) {
+			possuiEstacionamento = false;
+		}*/
+
+
+	}
 
 	public void removerSala() {}
 
